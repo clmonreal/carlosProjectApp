dc593486e654d9893a85e0b937a3ca071b12c3be
Aquiles18 <c.lopemonre@gmail,com>
2023-02-02 11:34:59 +0100

    Configura rutas de la carpeta views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { CreateComponent } from './views/create/create.component';
+import { EditComponent } from './views/edit/edit.component';
+import { ErrorComponent } from './views/error/error.component';
+import { HomeComponent } from './views/home/home.component';
+import { ListComponent } from './views/list/list.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'login', pathMatch:'full' },
+  { path:'about', component:AboutComponent },
+  { path:'contact', component:ContactComponent },
+  { path:'create', component:CreateComponent },
+  { path:'edit', component:EditComponent },
+  { path:'error', component:ErrorComponent },
+  { path:'home', component:HomeComponent },
+  { path:'list', component:ListComponent },
+  { path:'login', component:LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AboutComponent, ContactComponent, CreateComponent, EditComponent, ErrorComponent, HomeComponent, ListComponent, LoginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
-import { LoginComponent } from './views/login/login.component';
-import { HomeComponent } from './views/home/home.component';
-import { ListComponent } from './views/list/list.component';
-import { ContactComponent } from './views/contact/contact.component';
-import { ErrorComponent } from './views/error/error.component';
-import { AboutComponent } from './views/about/about.component';
-import { CreateComponent } from './views/create/create.component';
-import { EditComponent } from './views/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    HomeComponent,
-    ListComponent,
-    ContactComponent,
-    ErrorComponent,
-    AboutComponent,
-    CreateComponent,
-    EditComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
