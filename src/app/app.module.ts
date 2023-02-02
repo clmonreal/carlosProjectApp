89c2756a50669fe649a8212a4e2f1e2cde17d1a1
Aquiles18 <c.lopemonre@gmail,com>
2023-02-02 10:48:34 +0100

    crea componentes, servicios y modelos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { ListComponent } from './views/list/list.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { ErrorComponent } from './views/error/error.component';
+import { AboutComponent } from './views/about/about.component';
+import { CreateComponent } from './views/create/create.component';
+import { EditComponent } from './views/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    ListComponent,
+    ContactComponent,
+    ErrorComponent,
+    AboutComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
